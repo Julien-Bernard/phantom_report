eac0eae1f726de24b98a6c5b9bff511eaf31ef59
Julien Bernard <julien.bernard@nuance.com>
2018-05-25 10:59:06 -0400

    Initial commit

--- phantom_report.py
+++ phantom_report.py
@@ -3,3 +3,12 @@
 # __author__ = 'julien.bernard <julien.bernard@gmail.com>'
 # __description__ = 'Python script to create a timeline PDF report from Phantom.us'
 
+"""
+This scripts creates a report for a specific Phantom container
+"""
+
+import logging
+import json
+import requests
+from weasyprint import HTML,CSS
+
